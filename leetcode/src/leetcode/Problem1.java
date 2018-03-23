b2918f50956e063dfe034ae31927dc09bc5dc9f7
wu <1072124335@qq.com>
2018-03-23 13:53:25 +0800

    first

---
+++ leetcode/src/leetcode/Problem1.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * 1. Two Sum
+ * @author Wu
+ * 2018Äê3ÔÂ23ÈÕ
+ */
+public class Problem1 {
+	
+	class Solution {
+	    public int[] twoSum(int[] nums, int target) {
+	        int m=0;
+	        int n=0;
+	        for(int i=0;i<nums.length-1;i++){
+	            for(int j=i+1;j<nums.length;j++){
+	                if(nums[i]+nums[j]==target){
+	                    m=i;
+	                    n=j;
+	                }
+	            }
+	        }
+	        return new int[]{m,n};
+	    }
+	}
+}
---
+++ leetcode/src/leetcode/Problem2.java
@@ -0,0 +1,52 @@
+package leetcode;
+
+/**
+ * 2. Add Two Numbers
+ * @author Wu
+ * 2018Äê3ÔÂ23ÈÕ
+ */
+public class Problem2 {
+
+	
+	  public class ListNode {
+	      int val;
+	      ListNode next;
+	      ListNode(int x) { val = x; }
+	  }
+	 
+	class Solution {
+	    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+	        int i = 0;
+	        int left = 0;
+	        int sum = l1.val+l2.val+left;
+	        i = sum%10;
+	        left = sum/10;
+	        ListNode a = new ListNode(i);
+	        ListNode ret = a;
+	        l1 = l1.next;
+	        l2 = l2.next;
+	        while(l1!=null||l2!=null){
+	            int t1 = 0;
+	            int t2 = 0;
+	            if(l1!=null){
+	                t1 = l1.val;
+	                l1 = l1.next;
+	            }
+	            if(l2!=null){
+	                t2 = l2.val;
+	                l2 = l2.next;
+	            }
+	            sum = t1+t2+left;
+	            left = sum/10;
+	            i = sum%10;
+	            ListNode b = new ListNode(i);
+	            a.next = b;
+	            a = b;
+	        }
+	       if(left!=0){
+	        a.next = new ListNode(left);
+	       }
+	        return ret;
+	    }
+	}
+}
